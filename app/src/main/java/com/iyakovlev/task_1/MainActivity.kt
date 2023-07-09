e9ae52e62cc0b7385188a7f6ce03cdda8e5b5279
Ilya <ilesha0123@gmail.com>
2023-07-09 22:52:47 +0300

    auth screen changed to constraint (without theme)

--- app/src/main/java/com/iyakovlev/task_1/MainActivity.kt
+++ app/src/main/java/com/iyakovlev/task_1/MainActivity.kt
@@ -1,22 +1,48 @@
 package com.iyakovlev.task_1
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import com.iyakovlev.task_1.databinding.MainLayoutBinding
 
-class MainActivity : ComponentActivity() {
-    lateinit var binding: MainLayoutBinding
+class MainActivity : BaseActivity<MainLayoutBinding>(MainLayoutBinding::inflate) {
+
+    private lateinit var preferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = MainLayoutBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        val preferences = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE)
-        val name = preferences.getString(NAME, "Name")
-        val surname = preferences.getString(SURNAME, "Surname")
-
-        binding.nameText.text = "$name $surname"
+        preferences = getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE)
+        parseEmailToPrefs()
+        setNameInTextView()
 
     }
+
+    private fun parseEmailToPrefs() {
+        val email = preferences.getString(EMAIL, null)
+        if (email != null) {
+            val leftPart = email.split("@")[0]
+            var name = "Name"
+            var surname = "Surname"
+            if (leftPart.contains(".")) {
+                name = leftPart.split(".")[0].replaceFirstChar { it.uppercaseChar() }
+                surname = leftPart.split(".")[1].replaceFirstChar { it.uppercaseChar() }
+            }
+            preferences.edit()
+                .putString(NAME, name)
+                .putString(SURNAME, surname)
+                .apply()
+        } else {
+            preferences.edit()
+                .putString(NAME, "name") // TODO: strings
+                .putString(SURNAME, "surname") // TODO: strings
+                .apply()
+        }
+    }
+
+    private fun setNameInTextView() {
+        val name = preferences.getString(NAME, getString(R.string.default_name))
+        val lastname = preferences.getString(SURNAME, getString(R.string.default_lastname))
+        binding.nameText.text = "$name $lastname" // TODO:
+    }
+
 }
